717916c3845c8e52b1d6aa23afbe71c866f95c19
Christian Murphy <Christian.Murphy@asu.edu>
2014-07-06 12:12:48 -0700

    starting rewrite of learning objectives
    fixed data integrity complition error in course overview

--- grails-app/controllers/imodv6/CourseOverviewController.groovy
+++ grails-app/controllers/imodv6/CourseOverviewController.groovy
@@ -1,7 +1,9 @@
 package imodv6
 
+import org.springframework.dao.DataIntegrityViolationException
+
 class CourseOverviewController {
-	def show(Long id) {
+	def index(Long id) {
 		def imodInstance = Imod.get(id)
 		if (!imodInstance) {
 			flash.message = message(code: 'default.not.found.message', args: [message(code: 'imod.label', default: 'Imod'), imodInstance])
@@ -10,5 +12,53 @@ class CourseOverviewController {
 		}
 
 		[imodInstance: imodInstance]
+	}
+
+	def update(Long id, Long version) {
+		def imodInstance = Imod.get(id)
+		if (!imodInstance) {
+			flash.message = message(code: 'default.not.found.message', args: [message(code: 'imod.label', default: 'Imod'), imodInstance])
+			redirect(action: "list")
+			return
+		}
+
+		if (version != null) {
+			if (imodInstance.version > version) {
+				imodInstance.errors.rejectValue("version", "default.optimistic.locking.failure",
+						[message(code: 'imod.label', default: 'Imod')] as Object[],
+						"Another user has updated this Imod while you were editing")
+				render(view: "edit", model: [imodInstance: imodInstance])
+				return
+			}
+		}
+
+		imodInstance.properties = params
+
+		if (!imodInstance.save(flush: true)) {
+			render(view: "edit", model: [imodInstance: imodInstance])
+			return
+		}
+
+		flash.message = message(code: 'default.updated.message', args: [message(code: 'imod.label', default: 'Imod'), imodInstance])
+		redirect(action: "list")
+	}
+
+	def delete(Long id) {
+		def imodInstance = Imod.get(id)
+		if (!imodInstance) {
+			flash.message = message(code: 'default.not.found.message', args: [message(code: 'imod.label', default: 'Imod'), imodInstance])
+			redirect(action: "list")
+			return
+		}
+
+		try {
+			imodInstance.delete(flush: true)
+			flash.message = message(code: 'default.deleted.message', args: [message(code: 'imod.label', default: 'Imod'), imodInstance])
+			redirect(action: "list")
+		}
+		catch (DataIntegrityViolationException e) {
+			flash.message = message(code: 'default.not.deleted.message', args: [message(code: 'imod.label', default: 'Imod'), imodInstance])
+			redirect(action: "show", id: id)
+		}
 	}
 }
---
+++ grails-app/controllers/imodv6/LearningObjectiveController.groovy
@@ -0,0 +1,19 @@
+package imodv6
+
+class LearningObjectiveController {
+	def performance() {
+
+	}
+
+	def content() {
+
+	}
+
+	def condition() {
+
+	}
+
+	def criteria() {
+
+	}
+}
