a3adee7fd04653c15f7ddfd66346f838cca6cc89
Christian Murphy <Christian.Murphy@asu.edu>
2014-07-08 08:54:41 -0700

    added learning objective create action

--- grails-app/controllers/imodv6/LearningObjectiveController.groovy
+++ grails-app/controllers/imodv6/LearningObjectiveController.groovy
@@ -16,4 +16,21 @@ class LearningObjectiveController {
 	def criteria(Long id) {
 		[imodInstance: Imod.get(id)]
 	}
+
+	def create(Long id) {
+		params.remove('owner')
+		params.remove('owner.id')
+		params.put(
+			'owner.id',
+			id
+		)
+		def learningObjectiveInstance = new LearningObjective(params)
+		learningObjectiveInstance.save(
+			flush: true
+		)
+		redirect(
+			action: "performance",
+			id: id
+		)
+	}
 }
