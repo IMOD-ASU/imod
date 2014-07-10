8513ff92348984c159469750f8d4b5ed2d427b64
Christian Murphy <Christian.Murphy@asu.edu>
2014-07-10 09:05:06 -0700

    beggining to add comment descriptions of domain classes and their attributes

--- grails-app/domain/imodv6/Audience.groovy
+++ grails-app/domain/imodv6/Audience.groovy
@@ -1,10 +1,13 @@
 package imodv6
 
+/**
+ * TODO what is the purpose of this?
+ */
 class Audience {
 	String description
 
-    static constraints = {
-    }
+	static constraints = {
+	}
 
 	static mapping = {
 		version false
