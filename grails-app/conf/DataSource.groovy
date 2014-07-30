96d026ff1817ece2b3e4aa570c54cd6f1dbf14bb
Christian Murphy <Christian.Murphy@asu.edu>
2014-07-30 08:22:31 -0700

    upgrade to Grails 2.4.3

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -1,10 +1,11 @@
 dataSource {
     pooled = true
-    driverClassName = "org.postgresql.Driver"
-    username = "postgres"
-    password = "postgres"
+    driverClassName = 'org.postgresql.Driver'
+    username = 'postgres'
+    password = 'postgres'
 }
 hibernate {
+	flush.mode = 'manual'
     cache.use_second_level_cache = true
     cache.use_query_cache = false
     cache.region.factory_class = 'org.hibernate.cache.ehcache.EhCacheRegionFactory'
@@ -13,30 +14,30 @@ hibernate {
 environments {
     development {
         dataSource {
-            dbCreate = "update" // one of 'create', 'create-drop', 'update', 'validate', ''
-            url = "jdbc:postgresql:sample"
+            dbCreate = 'update' // one of 'create', 'create-drop', 'update', 'validate', ''
+            url = 'jdbc:postgresql:sample'
         }
     }
     test {
         dataSource {
-            dbCreate = "update"
-            url = "jdbc:postgresql:sample"
+            dbCreate = 'update'
+            url = 'jdbc:postgresql:sample'
         }
     }
     production {
         dataSource {
-            dbCreate = "update"
-            url = "jdbc:postgresql:sample"
+            dbCreate = 'update'
+            url = 'jdbc:postgresql:sample'
             pooled = true
             properties {
                maxActive = -1
-               minEvictableIdleTimeMillis=1800000
-               timeBetweenEvictionRunsMillis=1800000
-               numTestsPerEvictionRun=3
-               testOnBorrow=true
-               testWhileIdle=true
-               testOnReturn=true
-               validationQuery="SELECT 1"
+               minEvictableIdleTimeMillis = 1800000
+               timeBetweenEvictionRunsMillis = 1800000
+               numTestsPerEvictionRun = 3
+               testOnBorrow = true
+               testWhileIdle = true
+               testOnReturn = true
+               validationQuery = 'SELECT 1'
             }
         }
     }
