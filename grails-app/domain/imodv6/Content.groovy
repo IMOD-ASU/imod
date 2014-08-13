a203d260e0d75349c16520c18267a23d8f91e7c6
Christian Murphy <Christian.Murphy@asu.edu>
2014-08-13 09:38:02 -0700

    more readability fixes

--- grails-app/domain/imodv6/Content.groovy
+++ grails-app/domain/imodv6/Content.groovy
@@ -5,7 +5,7 @@ package imodv6
  * @param topicTitle the title of the content to be covered
  * @param piorityCode gives a text description of the important of a topic
  * @param knowledgeDomainCode TODO what is this?
- * @param preReq describes what a student should have knowledge of before starting topic
+ * @param preReq TODO: what is the boolean for?
  *
  * @param ContentResource a reference to the actual content for this topic ( TODO ? )
  * @param subTopic another topic that falls under the general over-arching topic
@@ -31,7 +31,7 @@ class Content {
 		Content
 	]
 
-	static List priorities(){
+	static List priorities() {
 		def priorityList = [
 			'Critical',
 			'Very Important',
@@ -56,12 +56,14 @@ class Content {
 	static mapping = {
 		version false
 	}
+
 	static transients = ['priorities']
-	def checkRecursion(currentContent, contentToAdd, errors){
-		if (currentContent == null){
+
+	def checkRecursion(currentContent, contentToAdd, errors) {
+		if (currentContent == null) {
 			return true
 		}
-		if (currentContent==contentToAdd){
+		if (currentContent == contentToAdd) {
 			errors.rejectValue('subContents', 'Cannot contain self')
 		}
 		else{
