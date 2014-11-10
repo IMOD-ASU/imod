5837f382ee678d2bf6fee573f843a7fc427c313a
Rahul Parekh <contact@rahulparekh.in>
2014-11-10 10:31:10 -0700

    Fixed registration email error

--- grails-app/controllers/security/RegisterController.groovy
+++ grails-app/controllers/security/RegisterController.groovy
@@ -17,6 +17,50 @@ class RegisterController extends grails.plugin.springsecurity.ui.RegisterControl
 	def messageSource
 	def saltSource
 
+	def index() {
+		def copy = [:] + (flash.chainedParams ?: [:])
+		copy.remove 'controller'
+		copy.remove 'action'
+		[command: new RegisterCommand(copy)]
+	}
+
+	def register(RegisterCommand command) {
+
+		if (command.hasErrors()) {
+			render view: 'index', model: [command: command]
+			return
+		}
+
+		String salt = saltSource instanceof NullSaltSource ? null : command.username
+		def user = lookupUserClass().newInstance(email: command.email, username: command.username,
+				accountLocked: true, enabled: true)
+
+		RegistrationCode registrationCode = springSecurityUiService.register(user, command.password, salt)
+		if (registrationCode == null || registrationCode.hasErrors()) {
+			// null means problem creating the user
+			flash.error = message(code: 'spring.security.ui.register.miscError')
+			flash.chainedParams = params
+			redirect action: 'index'
+			return
+		}
+
+		String url = generateLink('verifyRegistration', [t: registrationCode.token])
+
+		def conf = SpringSecurityUtils.securityConfig
+		def body = conf.ui.register.emailBody
+		if (body.contains('$')) {
+			body = evaluate(body, [user: user, url: url])
+		}
+		mailService.sendMail {
+			to command.email
+			from conf.ui.register.emailFrom
+			subject conf.ui.register.emailSubject
+			html body.toString()
+		}
+
+		render view: 'index', model: [emailSent: true]
+	}
+
 	def forgotPassword() {
 
 		if (!request.post) {
@@ -58,5 +102,41 @@ class RegisterController extends grails.plugin.springsecurity.ui.RegisterControl
 		}
 
 		[emailSent: true]
+	}
+}
+
+class RegisterCommand {
+
+	String username
+	String email
+	String password
+	String password2
+
+	def grailsApplication
+
+	static constraints = {
+		username blank: false, validator: { value, command ->
+			if (value) {
+				def User = command.grailsApplication.getDomainClass(
+					SpringSecurityUtils.securityConfig.userLookup.userDomainClassName).clazz
+				if (User.findByUsername(value)) {
+					return 'registerCommand.username.unique'
+				}
+			}
+		}
+		email blank: false, email: true
+		password blank: false, validator: RegisterController.passwordValidator
+		password2 validator: RegisterController.password2Validator
+	}
+}
+
+class ResetPasswordCommand {
+	String username
+	String password
+	String password2
+
+	static constraints = {
+		password blank: false, validator: RegisterController.passwordValidator
+		password2 validator: RegisterController.password2Validator
 	}
 }
