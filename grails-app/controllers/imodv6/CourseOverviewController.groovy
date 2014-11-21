8cbe2eb0aeef3ea4a92e5551100d2f816888f12b
Rahul Parekh <contact@rahulparekh.in>
2014-11-21 14:35:33 -0700

    controller fixes for add instructor

--- grails-app/controllers/imodv6/CourseOverviewController.groovy
+++ grails-app/controllers/imodv6/CourseOverviewController.groovy
@@ -9,4 +9,32 @@ class CourseOverviewController {
 			currentPage: 'course overview'
 		]
 	}
+
+    def springSecurityService
+
+    def create(){
+
+        def currentUser = ImodUser.findById(springSecurityService.currentUser.id)
+
+        // create a new instructor
+        def newInstructor = new Instructor(
+            firstName:'abc',
+            lastName:'xyz',
+            email:'abc@asu.edu',
+            role:'Professor',
+            location:'Peralta',
+            createdBy: currentUser
+        )
+
+        // save new instructor and the updated user to database
+        newInstructor.save()
+
+        // redirect to editing new Instructor
+        redirect(
+            controller: 'CourseOverview',
+            action: 'index',
+            id: springSecurityService.currentUser.id
+        )
+
+    }
 }
