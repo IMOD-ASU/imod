c88ab7e28d2e5f071e8b0589275b09444df8e357
Christian Murphy <Christian.Murphy@asu.edu>
2015-01-07 19:09:15 -0700

    comments explaining html lint setup

--- web-app/js/source/lintHTML.js
+++ web-app/js/source/lintHTML.js
@@ -2,24 +2,27 @@
 
 'use strict';
 
+// define rules for html hint
 var messages = HTMLHint.verify(
 	document.documentElement.innerHTML,
-{
-	"attr-lowercase": true,
-	"attr-no-duplication": true,
-	"attr-unsafe-chars": true,
-	"attr-value-double-quotes": true,
-	"attr-value-not-empty": true,
-	"doctype-html5": true,
-	"id-class-value": "dash",
-	"id-unique": true,
-	"space-tab-mixed-disabled": true,
-	"spec-char-escape": true,
-	"src-not-empty": true,
-	"tag-pair": true,
-	"tagname-lowercase": true,
-}
+	{
+		"attr-lowercase": true,
+		"attr-no-duplication": true,
+		"attr-unsafe-chars": true,
+		"attr-value-double-quotes": true,
+		"attr-value-not-empty": true,
+		"doctype-html5": true,
+		"id-class-value": "dash",
+		"id-unique": true,
+		"space-tab-mixed-disabled": true,
+		"spec-char-escape": true,
+		"src-not-empty": true,
+		"tag-pair": true,
+		"tagname-lowercase": true,
+	}
 );
+
+// add results to the end of the page
 document.documentElement.innerHTML += "<ul>";
 for (var i = 0; i < messages.length; i++) {
 	var current = messages[i];
