6d5468a49eeb1788638299b96a8f4861f38128b4
mak1909 <ayush.maheshwari19@gmail.com>
2015-01-12 01:37:12 -0700

    Reverting back datasource from create-drop to update

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -14,7 +14,7 @@ hibernate {
 environments {
 	development {
 		dataSource {
-			dbCreate = 'create-drop' // one of 'create', 'create-drop', 'update', 'validate', ''
+			dbCreate = 'update' // one of 'create', 'create-drop', 'update', 'validate', ''
 			url = 'jdbc:postgresql:sample'
 		}
 	}
