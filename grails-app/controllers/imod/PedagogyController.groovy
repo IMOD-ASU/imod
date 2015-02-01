cd5b044876bb6f01a7f1a213032ba7ef76ef0412
Christian Murphy <Christian.Murphy@asu.edu>
2015-02-01 16:26:54 -0700

    use learning objective service in pedagogy tab

--- grails-app/controllers/imod/PedagogyController.groovy
+++ grails-app/controllers/imod/PedagogyController.groovy
@@ -18,23 +18,10 @@ class PedagogyController {
 		def currentImod = Imod.get(id)
 
 		// finds all the learning objective linked to this imod
-		def learningObjectives = LearningObjective.findAllByImod(currentImod)
+		def learningObjectives = learningObjectiveService.getAllByImod(currentImod)
 
-		// Search for selected learning objective
-		// if there are no learning objectives set null
-		// if there is no selected learning objective, pick first
-		// otherwise get the objective
-		def currentLearningObjective
-		def objectivesForCurrentImod = currentImod.learningObjectives
-		if (objectivesForCurrentImod.size() < 1) {
-			currentLearningObjective = null
-		}
-		else if (learningObjectiveID == null) {
-			currentLearningObjective = objectivesForCurrentImod.first()
-		}
-		else {
-			currentLearningObjective = objectivesForCurrentImod.find { it.id == learningObjectiveID }
-		}
+		// select current learning objective
+		def currentLearningObjective = learningObjectiveService.safeGet(currentImod, learningObjectiveID)
 
 		// get all of the filters used to find pedagogy techniques
 		def domainCategories = DomainCategory.list()
