bdd3edc66c0bf3f8a4577475e198408e48504117
Christian Murphy <Christian.Murphy@asu.edu>
2015-03-12 09:49:02 -0700

    beautify javascript

--- web-app/js/source/courseOverview.js
+++ web-app/js/source/courseOverview.js
@@ -10,21 +10,21 @@ $(document).ready(function() {
 	);
 
 	// delete instructor logic
-	$('.delete-instructor').click(function(){
+	$('.delete-instructor').click(function() {
 
-	    var ids = [];
-		$('.instructor-list').find('.topicListRow.selected').each(function(){
+		var ids = [];
+		$('.instructor-list').find('.topicListRow.selected').each(function() {
 			ids.push($(this).data('id'));
 		});
 
-		if(ids.length < 1){
-			alert("Please select an instructor to delete");
+		if (ids.length < 1) {
+			alert('Please select an instructor to delete');
 			return false;
 		}
 
-		if (confirm("Are you sure you want to delete the instructor(s)") == false) {
-	        return false;
-	    }
+		if (confirm('Are you sure you want to delete the instructor(s)') === false) {
+			return false;
+		}
 
 		$.ajax({
 			url: baseUrl + 'courseOverview/delete',
@@ -34,8 +34,8 @@ $(document).ready(function() {
 				imod_id: $('#imodID').val(),
 				selected: ids
 			},
-			success: function(data){
-				location.reload()
+			success: function(data) {
+				location.reload();
 			},
 			error: function(xhr) {
 				// when something goes wrong log to the browser console
@@ -57,25 +57,25 @@ $(document).ready(function() {
 	$('.topicButtonGradient .remove').click(function(){
 
 		if ($('#topicList tbody tr').length > 1){
-			$('#topicList tbody tr:last').remove();	
-		}		
-		
+			$('#topicList tbody tr:last').remove();
+		}
+
 		return false;
 	});*/
 
 	$('#topicList > tbody').on('click', 'tr', toggleSelected);
 
-	$('.topicButtonGradient .add').click(function(){
-		var row="";
+	$('.topicButtonGradient .add').click(function() {
+		var row = "";
 		row += "<tr class=\"topicListRow\">";
-		row += "    <td class=\"saveIcon\">";
-		row += "        <i class=\"hidden fa fa-eraser\"><\/i>";
-		row += "    <\/td>";
-		row += "    <td><input type=\"text\" name=\"firstName\" value=\"\" id=\"firstName\"  class=\"first_name\" \/><\/td>";
-		row += "    <td><input type=\"text\" name=\"lastName\" value=\"\" id=\"lastName\"  class=\"last_name\" \/><\/td>";
-		row += "    <td><input type=\"text\" name=\"email\" value=\"\" id=\"email\"  class=\"email\" \/><\/td>";
-		row += "    <td><input type=\"text\" name=\"officeHours\" value=\"\" id=\"officeHours\" class=\"office_hours\" \/><\/td>";
-		row += "    <td><input type=\"text\" name=\"webPage\" value=\"\" id=\"webPage\" class=\"web_page\" \/><\/td>";
+		row += "	<td class=\"saveIcon\">";
+		row += "		<i class=\"hidden fa fa-eraser\"><\/i>";
+		row += "	<\/td>";
+		row += "	<td><input type=\"text\" name=\"firstName\" value=\"\" id=\"firstName\"  class=\"first_name\" \/><\/td>";
+		row += "	<td><input type=\"text\" name=\"lastName\" value=\"\" id=\"lastName\"  class=\"last_name\" \/><\/td>";
+		row += "	<td><input type=\"text\" name=\"email\" value=\"\" id=\"email\"  class=\"email\" \/><\/td>";
+		row += "	<td><input type=\"text\" name=\"officeHours\" value=\"\" id=\"officeHours\" class=\"office_hours\" \/><\/td>";
+		row += "	<td><input type=\"text\" name=\"webPage\" value=\"\" id=\"webPage\" class=\"web_page\" \/><\/td>";
 		row += "<td>";
 		row += "	<select name=\"role\" id=\"role\" class=\"role\">";
 		row += "		<option value=\"\">Select Role<\/option>";
@@ -84,7 +84,7 @@ $(document).ready(function() {
 		row += "		<option>Grader<\/option>";
 		row += "	<\/select>";
 		row += "<\/td>";
-		row += "    <td><input type=\"text\" name=\"location\" value=\"\" id=\"location\" class=\"location\" \/><\/td>";
+		row += "	<td><input type=\"text\" name=\"location\" value=\"\" id=\"location\" class=\"location\" \/><\/td>";
 		row += "<\/tr>";
 
 		$('#topicList tbody').append(row);
@@ -94,23 +94,23 @@ $(document).ready(function() {
 		return false;
 	});
 
-	$('#cancelTopic').click(function(){
-		$('#topicDialogBackground, #topicDialog').hide()
+	$('#cancelTopic').click(function() {
+		$('#topicDialogBackground, #topicDialog').hide();
 		return false;
 	});
 
-	$('.save-instructors').click(function(){
+	$('.save-instructors').click(function() {
 
 		var isValid = $('form.instructor-form').valid();
 
-		if(isValid){
+		if (isValid) {
 
 			var parameterList = [];
 
-			$('.topicListRow').each(function(){
+			$('.topicListRow').each(function() {
 				var row = $(this);
 				console.log(row.data('id'));
-				if(!row.data('id')){
+				if (!row.data('id')) {
 					parameterList.push({
 						lastName: row.find('input[name=lastName]').val(),
 						firstName: row.find('input[name=firstName]').val(),
@@ -118,10 +118,10 @@ $(document).ready(function() {
 						officeHours: row.find('input[name=officeHours]').val(),
 						webPage: row.find('input[name=webPage]').val(),
 						role: row.find('select[name=role]').val(),
-						location: row.find('input[name=location]').val(),
+						location: row.find('input[name=location]').val()
 					});
 				}
-				
+
 			});
 
 
@@ -133,8 +133,8 @@ $(document).ready(function() {
 					imod_id: $('input[name=id]').val(),
 					parameters: JSON.stringify(parameterList)
 				},
-				success: function(data){
-					location.reload()
+				success: function(data) {
+					location.reload();
 				},
 				error: function(xhr) {
 					// when something goes wrong log to the browser console
@@ -146,112 +146,110 @@ $(document).ready(function() {
 
 		return false;
 	});
-	
+
 	// instructor validation
 	jQuery.validator.addClassRules({
-	  first_name: {
-	    required: true,
-	  },
-	  last_name: {
-	    required: true,
-	  },
-	  email: {
-	    required: true,
-	    email: true,
-	  },
-	  role: {
-	  	required: true,
-	  }
+		first_name: {
+			required: true
+		},
+		last_name: {
+			required: true
+		},
+		email: {
+			required: true,
+			email: true
+		},
+		role: {
+			required: true
+		}
 	});
 
 	// course overview validation
-	$(".courseoverview").validate({
-	  	rules: {
-	    	imodNumber: "required",
-	    	name:{
-	    		required: true
-	    	},	  
-	    	url: {
-	    		required: true,
+	$('.courseoverview').validate({
+		rules: {
+			imodNumber: 'required',
+			name: {
+				required: true
+			},
+			url: {
+				required: true,
 				url: true
-	    	},
-	    	numberOfSeats: {
-	    		digits: true
-	    	},
-	    	subjectArea: {
-	    		required: true
-	    	}
-	  	},
-	  	messages: {
-	    	url: {
-	      		url: "Please enter a valid URL eg. http://google.com"
-	    	}
+			},
+			numberOfSeats: {
+				digits: true
+			},
+			subjectArea: {
+				required: true
+			}
+		},
+		messages: {
+			url: {
+				url: 'Please enter a valid URL eg. http://google.com'
+			}
 		}
 	});
 
 	// instructor form validation
-	$('.instructor-form').validate()
-	
+	$('.instructor-form').validate();
+
 	gradingRadio($('.grading-radio:checked'));
 
 	// grading procedure radio buttons
-	$('.grading-radio').change(function(){
+	$('.grading-radio').change(function() {
 		gradingRadio($(this));
 	});
 
-	$('.courseoverview').submit(function(){
+	$('.courseoverview').submit(function() {
 		var radio = $('.grading-radio:checked');
 
-		if(radio.val() != 'Custom'){
+		if (radio.val() !== 'Custom') {
 			$('#grading-procedure-text').val(radio.val());
 		}
 	});
 
 });
 
-function gradingRadio(radio){
+function gradingRadio(radio) {
 	$('#grading-procedure-text').hide();
-	if(radio.val() == 'Custom'){
+	if (radio.val() === 'Custom') {
 		$('#grading-procedure-text').show();
 	}
-	radio.parents('.ui-accordion-content').css('height','auto');
+	radio.parents('.ui-accordion-content').css('height', 'auto');
 }
 
 function populateRepeatsEvery() {
-	if($('#repeats option:selected').text() === "Daily") {
+	if ($('#repeats option:selected').text() === "Daily") {
 		$('#duration').text("days");
-		$('#duration').css('visibility','visible');
+		$('#duration').css('visibility', 'visible');
 		$('label[for="repeatsEvery"]').css('visibility', 'visible');
-		$('#repeatsEvery').css('visibility','visible');
+		$('#repeatsEvery').css('visibility', 'visible');
 		$('label[for="scheduleWeekDays"]').css('visibility', 'hidden');
 		$('label[for="weekdays"]').css('visibility', 'hidden');
 		$(':checkbox').removeAttr('checked');
-		$(':checkbox').css('visibility','hidden');
+		$(':checkbox').css('visibility', 'hidden');
 
-	}
-	else if($('#repeats option:selected').text() === "Weekly") {
+	} else if ($('#repeats option:selected').text() === "Weekly") {
 		$('#duration').text("weeks");
-		$('#duration').css('visibility','visible');
+		$('#duration').css('visibility', 'visible');
 		$('label[for="repeatsEvery"]').css('visibility', 'visible');
-		$('#repeatsEvery').css('visibility','visible');
+		$('#repeatsEvery').css('visibility', 'visible');
 		$('label[for="scheduleWeekDays"]').css('visibility', 'visible');
 		$('label[for="weekdays"]').css('visibility', 'visible');
-		$(':checkbox').css('visibility','visible');
-	}
-	else {
+		$(':checkbox').css('visibility', 'visible');
+	} else {
 		$('label[for="repeatsEvery"]').css('visibility', 'hidden');
-		$('#repeatsEvery').css('visibility','hidden');
-		$('#duration').css('visibility','hidden');
+		$('#repeatsEvery').css('visibility', 'hidden');
+		$('#duration').css('visibility', 'hidden');
 		$('label[for="scheduleWeekDays"]').css('visibility', 'hidden');
 		$('label[for="weekdays"]').css('visibility', 'hidden');
 		$(':checkbox').removeAttr('checked');
-		$(':checkbox').css('visibility','hidden');
+		$(':checkbox').css('visibility', 'hidden');
 	}
 }
 
-function toggleSelected(event){
-	if (!(event.target.nodeName in ['OPTION', 'INPUT', 'BUTTON','SELECT'])){
-		$(this).find(".saveIcon > i").toggleClass("hidden");
-		$(this).toggleClass("selected");
+function toggleSelected(event) {
+	if (!(event.target.nodeName in ['OPTION', 'INPUT', 'BUTTON', 'SELECT'])) {
+		$(this).find('.saveIcon > i').toggleClass('hidden');
+		$(this).toggleClass('selected');
 	}
 }
--- web-app/js/source/learningObjectiveContent.js
+++ web-app/js/source/learningObjectiveContent.js
@@ -1,12 +1,12 @@
 'use strict';
 
 $(function() {
-	var jsonData=JSON.parse($("input[name=treeData]").val());
-	buildContentTree(jsonData,false);
-	$("#contentTree").on("ready.jstree",function() {
-		var idList=$("#contentTree").find("li.topicSelected");
-		$(idList).each(function(){
-			refreshTreeValues($(this).children("a"),false);
+	var jsonData = JSON.parse($("input[name=treeData]").val());
+	buildContentTree(jsonData, false);
+	$("#contentTree").on("ready.jstree", function() {
+		var idList = $("#contentTree").find("li.topicSelected");
+		$(idList).each(function() {
+			refreshTreeValues($(this).children("a"), false);
 		});
 		var newJSONdata = $("#contentTree").jstree(true).get_json();
 		$("#contentTree").jstree("destroy");
@@ -14,166 +14,158 @@ $(function() {
 	});
 });
 
-function buildContentTree(jsonData, refreshDB){
+function buildContentTree(jsonData, refreshDB) {
 	$("#contentTree").jstree({
 		"core": {
-			"data":jsonData,
+			"data": jsonData,
 			"check_callback": true,
 			"themes": {
 				"icons": false
 			}
 		},
-		"dnd":{
+		"dnd": {
 			"copy": false
 		},
-		"plugins":["wholerow", "dnd", "types"]
+		"plugins": ["wholerow", "dnd", "types"]
 	});
-	$("#contentTree").on('ready.jstree', function(){
+	$("#contentTree").on('ready.jstree', function() {
 		$("#contentTree").jstree('open_all');
 
-		$("#contentTree .jstree-wholerow").on("click", function(){
+		$("#contentTree .jstree-wholerow").on("click", function() {
 			selectCheckboxes(this);
 		});
-		$("#contentTree .jstree-anchor").on("click", function(){
+		$("#contentTree .jstree-anchor").on("click", function() {
 			selectCheckboxes(this);
 		});
 		refreshCheckboxes(refreshDB);
 	});
-	$("#contentTree").on("move_node.jstree",function(e, data){
+	$("#contentTree").on("move_node.jstree", function(e, data) {
 		$("#contentTree").jstree("open_node", "#" + data.node.parent);
-		refreshTreeValues($("#"+data.node.id).children("a"), true);
+		refreshTreeValues($("#" + data.node.id).children("a"), true);
 		moveContent(data.node.id.substr(7), data.node.parent.substr(7));
 	});
 	$("#contentTree").off("click.jstree");
 }
 
-function selectCheckboxes(currentRow){
+function selectCheckboxes(currentRow) {
 	var contentID = $(currentRow).parent("li").attr('id');
 	var contentNode = $('#contentTree').jstree(true).get_node(contentID);
 	var contentNodeClass = contentNode.li_attr["class"];
-	var testingObject = $("#"+contentID);
-	if (contentNodeClass == "topicNotSelected" || contentNodeClass == "topicIndeterminate"){
+	var testingObject = $("#" + contentID);
+	if (contentNodeClass === "topicNotSelected" || contentNodeClass === "topicIndeterminate") {
 		contentNode.li_attr["class"] = "topicSelected";
-		testingObject.find("li").each(function(){
-			var childNode = $('#contentTree').jstree(true).get_node($(this).attr('id'));
-			childNode.li_attr["class"]="topicSelected";
-		});
-	}
-	else{
-		contentNode.li_attr["class"]="topicNotSelected";
 		testingObject.find("li").each(function() {
-			var childNode=$('#contentTree').jstree(true).get_node($(this).attr('id'));
-			childNode.li_attr["class"]="topicNotSelected";
+			var childNode = $('#contentTree').jstree(true).get_node($(this).attr('id'));
+			childNode.li_attr["class"] = "topicSelected";
+		});
+	} else {
+		contentNode.li_attr["class"] = "topicNotSelected";
+		testingObject.find("li").each(function() {
+			var childNode = $('#contentTree').jstree(true).get_node($(this).attr('id'));
+			childNode.li_attr["class"] = "topicNotSelected";
 		});
 	}
-	refreshTreeValues(currentRow,true);
+	refreshTreeValues(currentRow, true);
 }
 
-function refreshTreeValues(currentRow, doRefresh){
-	var contentID=$(currentRow).parent("li").attr('id');
-	var contentNode=$('#contentTree').jstree(true).get_node(contentID);
+function refreshTreeValues(currentRow, doRefresh) {
+	var contentID = $(currentRow).parent("li").attr('id');
+	var contentNode = $('#contentTree').jstree(true).get_node(contentID);
 	var newJSONdata;
-	var indeterminate=false;
-	var currentNode=$('#contentTree').jstree(true).get_node(contentNode);
-	while (currentNode.parents.length>1){
-		currentNode=$('#contentTree').jstree(true).get_node(currentNode.parent);
-		if(currentNode.parents.length === 1) {
-			currentNode.a_attr["class"]="rootNode";
+	var indeterminate = false;
+	var currentNode = $('#contentTree').jstree(true).get_node(contentNode);
+	while (currentNode.parents.length > 1) {
+		currentNode = $('#contentTree').jstree(true).get_node(currentNode.parent);
+		if (currentNode.parents.length === 1) {
+			currentNode.a_attr["class"] = "rootNode";
+		} else {
+			currentNode.a_attr["class"] = "";
 		}
-		else {
-			currentNode.a_attr["class"]="";
-		}
-		if(!indeterminate){
+		if (!indeterminate) {
 			var numIndeterminate;
-			$(currentNode.children_d).each(function(){
-				if($('#contentTree').jstree(true).get_node(this).li_attr["class"]==="topicIndeterminate"){
+			$(currentNode.children_d).each(function() {
+				if ($('#contentTree').jstree(true).get_node(this).li_attr["class"] === "topicIndeterminate") {
 					numIndeterminate++;
 				}
 			});
-			if(numIndeterminate>0){
-				indeterminate=true;
-				currentNode.li_attr["class"]="topicIndeterminate";
-			}
-			else{
-				var treeSize=currentNode.children_d.length;
-				var numSelected=0;
-				$(currentNode.children_d).each(function(){
+			if (numIndeterminate > 0) {
+				indeterminate = true;
+				currentNode.li_attr["class"] = "topicIndeterminate";
+			} else {
+				var treeSize = currentNode.children_d.length;
+				var numSelected = 0;
+				$(currentNode.children_d).each(function() {
 					if ($('#contentTree').jstree(true).get_node(this).li_attr["class"] === "topicSelected") {
 						numSelected++;
 					}
 				});
-				if (treeSize !== numSelected && numSelected > 0){
-					indeterminate=true;
-					currentNode.li_attr["class"]="topicIndeterminate";
-				}
-				else if (treeSize === numSelected && numSelected > 0){
-					currentNode.li_attr["class"]="topicSelected";
-				}
-				else{
-					currentNode.li_attr["class"]="topicNotSelected";
+				if (treeSize !== numSelected && numSelected > 0) {
+					indeterminate = true;
+					currentNode.li_attr["class"] = "topicIndeterminate";
+				} else if (treeSize === numSelected && numSelected > 0) {
+					currentNode.li_attr["class"] = "topicSelected";
+				} else {
+					currentNode.li_attr["class"] = "topicNotSelected";
 				}
 			}
-		}
-		else{
-			currentNode.li_attr["class"]="topicIndeterminate";
+		} else {
+			currentNode.li_attr["class"] = "topicIndeterminate";
 		}
 	}
-	if(doRefresh){
-		newJSONdata=$("#contentTree").jstree(true).get_json();
+	if (doRefresh) {
+		newJSONdata = $("#contentTree").jstree(true).get_json();
 		$("#contentTree").jstree("destroy");
 		buildContentTree(newJSONdata, true);
 	}
 }
 
-function refreshCheckboxes(refreshDB){
-	var nodeArray=$("#contentTree").jstree(true)._model.data["#"].children_d;
-	var defaultClass="fa fa-stack-1x checkbox";
-	var idArray=[];
-	$(nodeArray).each(function(){
-		var icon=$("#select"+this.substr(7));
-		if ($("#"+this).hasClass("topicSelected")){
-			icon.attr("class",defaultClass+" fa-check");
+function refreshCheckboxes(refreshDB) {
+	var nodeArray = $("#contentTree").jstree(true)._model.data["#"].children_d;
+	var defaultClass = "fa fa-stack-1x checkbox";
+	var idArray = [];
+	$(nodeArray).each(function() {
+		var icon = $("#select" + this.substr(7));
+		if ($("#" + this).hasClass("topicSelected")) {
+			icon.attr("class", defaultClass + " fa-check");
 			idArray.push(this.substr(7));
-		}
-		else if ($("#"+this).hasClass("topicIndeterminate")){
-			icon.attr("class",defaultClass+" fa-minus");
-		}
-		else{
-			icon.attr("class",defaultClass);
+		} else if ($("#" + this).hasClass("topicIndeterminate")) {
+			icon.attr("class", defaultClass + " fa-minus");
+		} else {
+			icon.attr("class", defaultClass);
 		}
 	});
-	if (refreshDB){
+	if (refreshDB) {
 		setContents(idArray);
 	}
 }
 
-function moveContent(contentID, parentID){
+function moveContent(contentID, parentID) {
 	$.ajax({
-		url:"../../content/updateHierarchy",
-		type:"POST",
-		dataType:"json",
+		url: "../../content/updateHierarchy",
+		type: "POST",
+		dataType: "json",
 		data: {
 			contentID: contentID,
 			parentID: parentID
 		},
-		error: function(xhr){
+		error: function(xhr) {
 			alert(xhr.responseText);
 		}
 	});
 }
 
-function setContents(idArray){
-	var objectiveID=$("input[name=learningObjectiveID]").val();
-	idArray=JSON.stringify(idArray);
+function setContents(idArray) {
+	var objectiveID = $("input[name=learningObjectiveID]").val();
+	idArray = JSON.stringify(idArray);
 	$.ajax({
-		url:"../../content/setLearningObjective",
-		type:"POST",
-		dataType:"json",
+		url: "../../content/setLearningObjective",
+		type: "POST",
+		dataType: "json",
 		data: {
 			objectiveID: objectiveID,
 			idArray: idArray
 		},
-		error: function(xhr){
+		error: function(xhr) {
 			alert(xhr.responseText);
 		}
 	});
@@ -181,13 +173,12 @@ function setContents(idArray){
 
 // FIXME if unused remove topicList parameter
 function populateTopics(topicList) {
-	var contentTree=$.jstree.reference("#contentTree");
+	var contentTree = $.jstree.reference("#contentTree");
 	$("#topicList tbody").html("");
-	$(topicList).each(function(){
+	$(topicList).each(function() {
 		contentTree.create_node(
-			"#",
-			{
-				"id": "content"+this.contentID,
+			"#", {
+				"id": "content" + this.contentID,
 				"text": this.topicTitle
 			}
 		);
