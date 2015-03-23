ba5d3a39f2c2281efeb46d519fe70ebfa28aa1d1
Rahul Parekh <contact@rahulparekh.in>
2015-03-23 01:46:06 -0700

    program specification changed to problem specification

--- grails-app/domain/imod/LearningObjective.groovy
+++ grails-app/domain/imod/LearningObjective.groovy
@@ -61,7 +61,7 @@ class LearningObjective {
 	]
 
 	public static final List genericConditions = [
-		'Given a program specification',
+		'Given a problem specification',
 		'Students completing this course will be able to',
 		'After completing the course, the student will be able to'
 	]
