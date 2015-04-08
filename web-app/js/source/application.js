de33684327f061a622bc9480ef55efea532f92d1
mak1909 <ayush.maheshwari19@gmail.com>
2015-04-07 17:06:49 -0700

    quicktips: Show and Hide Implementation complete

--- web-app/js/source/application.js
+++ web-app/js/source/application.js
@@ -4,6 +4,7 @@ $(document).ready(function() {
 	$('#accordion').accordion();
 	$('#help-placeholder').draggable();
 	$('#open-help').draggable();
+	$('#help-placeholder-2').css('display', 'none');
 	$('.show-hover-new').qtip({
 		style: {
 			background: 'transparent',
@@ -20,17 +21,6 @@ $(document).ready(function() {
 	$('*').focus(function() {
 		//alert(document.activeElement);
 		$('#qtip-place').html(document.activeElement.title);
-	});
-	$('#QuickTips').click(function() {
-		var helpbox = $('#help-placeholder');
-		var tabContainer = $('#tabs-container');
-		if (helpbox.css('display') === 'block') {
-			helpbox.css('display', 'none');
-			tabContainer.css('margin-right', '2.5%');
-		} else {
-			helpbox.css('display', 'block');
-			tabContainer.css('margin-right', '17.5%');
-		}
 	});
 
 	$('.learning-objective.list-item').each(function() {
@@ -128,22 +118,15 @@ function updateDatePicker(dateFormat) {
 
 function toggleHelp() {
 	var helpbox = $('#help-placeholder');
-	var openbox = $('#open-help');
-	var boxtop;
-	var boxleft;
+	var helpbox2 = $('#help-placeholder-2');
+	var tabContainer = $('#tabs-container');
 	if (helpbox.css('display') === 'block') {
-		boxtop = helpbox.css('top');
-		boxleft = helpbox.css('left');
 		helpbox.css('display', 'none');
-		openbox.css('display', 'block');
-		openbox.css('top', boxtop);
-		openbox.css('left', boxleft);
+		helpbox2.css('display', 'block');
+		tabContainer.css('margin-right', '5.5%');
 	} else {
-		boxtop = openbox.css('top');
-		boxleft = openbox.css('left');
 		helpbox.css('display', 'block');
-		openbox.css('display', 'none');
-		helpbox.css('top', boxtop);
-		helpbox.css('left', boxleft);
+		helpbox2.css('display', 'none');
+		tabContainer.css('margin-right', '17.5%');
 	}
 }
