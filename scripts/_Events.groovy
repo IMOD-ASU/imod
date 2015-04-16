fbbef529c134087d4ffd8e7bdfcbd8d4dad89700
Christian Murphy <Christian.Murphy.42@gmail.com>
2015-04-16 11:25:52 -0700

    bower fixed on windows

--- scripts/_Events.groovy
+++ scripts/_Events.groovy
@@ -2,8 +2,14 @@ eventCompileStart = {kind ->
 	bowerInstallPackages()
 }
 
-private void bowerInstallPackages(){
-	def proc = 'bower install'.execute()  // execute default task to load dependencies from local cache.
+private void bowerInstallPackages() {
+	def proc
+	if (System.properties['os.name'].toLowerCase().contains('windows')) {
+		proc = ['cmd', '/c', 'bower install'].execute()
+	} else {
+		proc = 'bower install'.execute()
+	}
+
 	proc.waitFor()
 	if(proc.exitValue() != 0){
 		println 'An Error Occurred'
