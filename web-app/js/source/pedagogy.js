a21b194895c5d2b5c3b09d3975c1f6a8904f2555
Rahul Parekh <contact@rahulparekh.in>
2015-12-30 23:09:38 +0530

    Test server show hide favorites bugfix

--- web-app/js/source/pedagogy.js
+++ web-app/js/source/pedagogy.js
@@ -719,8 +719,8 @@ $(document).ready(
 
 		// Open favorite techniques
 		$('#favorites-button').click(function () {
-			$('.modalBackgroundFavorites').show();
-			$('.favorites-modal').show();
+			$('.modalBackgroundFavorites').css('display', 'block');
+			$('.favorites-modal').css('display', 'block');
 
 			$.ajax({
 				url: '../../pedagogyTechnique/favorites/',
@@ -734,8 +734,8 @@ $(document).ready(
 		});
 
 		$('.modalBackgroundFavorites').click(function () {
-			$('.modalBackgroundFavorites').hide();
-			$('.favorites-modal').hide();
+			$('.modalBackgroundFavorites').css('display', 'none');
+			$('.favorites-modal').css('display', 'none');
 		});
 
 		$(document)
