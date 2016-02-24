9a6a57bdbb9ac32348748a7204e132863cba7b05
Abir Saha <abir.saha@asu.edu>
2016-02-24 00:45:54 -0700

    Cancel button in LO-content subtab not working issue fixed

--- web-app/js/source/topicDialog.js
+++ web-app/js/source/topicDialog.js
@@ -506,7 +506,7 @@ function revertChanges () {
 	'use strict';
 	$('.topicItem').each(
 		function () {
-			if (this.id === 'undefined') {
+			if ($('#topicTempID'+this.id).val() === undefined) {
 				$(this).remove();
 			}
 		}
