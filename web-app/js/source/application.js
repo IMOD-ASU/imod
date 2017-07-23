8d36ea5329bdf82328108815a9f919f4da960041
Abir Saha <abir.saha@asu.edu>
2017-07-23 00:43:02 -0700

    Pushing progress bar changes_inline space fix

--- web-app/js/source/application.js
+++ web-app/js/source/application.js
@@ -144,7 +144,7 @@ function calculateContent (contents) {
 
 	if (count < 2) {
 		contentPercent = 0;
-	} else if (count >=2 && count <= 5) {
+	} else if (count >= 2 && count <= 5) {
 		contentPercent = 80;
 	} else {
 		contentPercent = 100;
